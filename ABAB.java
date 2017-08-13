d96994489d576d4dd3753c1eead481159b2b023a
lijiazhen1994 <2681066209@qq.com>
2017-08-13 09:22:01 +0800

    网易内推笔试题（测试开发工程师）

---
+++ ABAB.java
@@ -0,0 +1,41 @@
+package test;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class ABAB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner n = new Scanner(System.in);
+		String str=n.next();
+		//String str ="2342asfghgyu56asdasda";        
+		HashMap<String,Integer> maps = new HashMap<String,Integer>();  
+		for(int i=0;i<str.length();i++){         
+			String key = String.valueOf((str.charAt(i)));    
+			if(!maps.containsKey(key))  
+				maps.put(key, 1);     
+			else{        
+				int val =maps.get(key);     
+				maps.put(key, val+1);        
+				}               
+			}  
+		if(maps.size()>=3||maps.size()==0)
+		{
+			System.out.println("0");   
+		}
+		else if(maps.size()==2)
+		{
+			System.out.println("2");   
+		}
+		else 
+		{
+			System.out.println("1");  
+			
+		}
+		
+
+
+	}
+
+}
