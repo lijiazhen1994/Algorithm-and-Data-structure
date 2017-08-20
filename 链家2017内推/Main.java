bfd701984ef4630cd0667dbf63b4e74f0366a503
lijiazhen1994 <2681066209@qq.com>
2017-08-19 20:56:39 -0500

    Add files via upload

---
+++ 链家2017内推/Main.java
@@ -0,0 +1,59 @@
+package exercise;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in 
+
+				);
+						int N = sc.nextInt();
+						int[] arr = new int[N];
+						int ones = 0, twos = 0, threes = 0;
+						for(int i = 0; i < N; i++){
+							arr[i] = sc.nextInt();
+							if(arr[i] == 1){
+								ones++;
+							}else if(arr[i] == 2){
+								twos++;
+							}else{
+								threes++;
+							}
+						}
+						int count = 0;
+						int i = 0,j = 0, k = 0;
+						while(j < ones){
+							if(arr[i] == 1){
+								j++;
+								if(i != k){
+									arr[i] = arr[k];
+									arr[k] = 1;
+									count++;
+								}
+								k++;
+							}
+							i++;
+						}
+//						System.out.println(count);
+						i = ones;
+						j = 0;
+						k = ones;
+						while(j < twos){
+							if(arr[i] == 2){
+								j++;
+								if(i != k){
+									arr[i] = arr[k];
+									arr[k] = 2;
+									count++;
+								}
+								k++;
+							}
+							i++;
+						}
+						System.out.println(count);
+	}
+
+}
